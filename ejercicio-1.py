57a52a4c7d25a520be1a109af11b32a72dc1439b
JesusGarnica8684 <lgarciagar@unal.edu.co>
2025-03-01 15:13:50 -0500

    ad

---
+++ ejercicio-1.py
@@ -0,0 +1,9 @@
+if __name__=="__main__":
+    dict_edades = {"julian":29, "jesus":19, "ale":25} # se crea un diccionario de edades segun persona
+    dictdefault.dict_edades 
+    l_edades = list(dict_edades.values())  # se crea una lista donde solo esten las edades de las personas
+    l_edades.sort()
+    n_elista : int  = (len(l_edades))
+    for i in range (n_elista-1):
+        
+    print(l_edades)
