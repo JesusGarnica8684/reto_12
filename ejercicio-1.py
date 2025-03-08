6e48a5a94cdfd0ee55fc5fd1db47721c9a91f773
JesusGarnica8684 <lgarciagar@unal.edu.co>
2025-03-07 20:50:34 -0500

    add

--- ejercicio-1.py
+++ ejercicio-1.py
@@ -1,9 +1,5 @@
-if __name__=="__main__":
-    dict_edades = {"julian":29, "jesus":19, "ale":25} # se crea un diccionario de edades segun persona
-    dictdefault.dict_edades 
-    l_edades = list(dict_edades.values())  # se crea una lista donde solo esten las edades de las personas
-    l_edades.sort()
-    n_elista : int  = (len(l_edades))
-    for i in range (n_elista-1):
-        
+if __name__ == "__main__":
+    dict_edades = {"julian": 29, "jesus": 19, "ale": 25}
+    l_edades = list(dict_edades.values()) # se convierte el diccionario a una lista
+    l_edades.sort() # se organizan ascendentemente los valores 
     print(l_edades)
